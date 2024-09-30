7dd0f3beda82818e3fb9c03ccb97c6680d16a2ea
sindhugowda31 <sindhugowda31@gmail.com>
2024-09-30 17:42:42 +0530

    I added the addition code.

---
+++ ecoders_automation_testing/src/basics/Addition.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Addition {
+
+	public static void main(String[] args) {
+	    int a = 10; 
+	    int b = 20;
+	    
+	    System.out.println("The sum of two numbers is "+ (a+b));
+
+	}
+
+}
